7c75a3887e0d047a343ad661c29bc425d73034fd
Ashishkumar90255 <143105783+Ashishkumar90255@users.noreply.github.com>
2025-05-27 12:19:31 +0000

    04_comparison.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,8 +15,15 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Ashish"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
-// "" =
+// "" => false
+//"Ashish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
