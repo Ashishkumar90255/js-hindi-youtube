d1fd0e3efbb535c597b34c2a77b2e3b732ab8a8f
Ashishkumar90255 <143105783+Ashishkumar90255@users.noreply.github.com>
2025-05-27 13:14:08 +0000

    04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (Strict check, it check its values as well as a its datatype also)
+console.log("2" === 2);
+
+
+
+
+
+
