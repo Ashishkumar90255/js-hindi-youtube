19167367b0dc32d966247db29f4403a1181a2f8d
Ashishkumar90255 <143105783+Ashishkumar90255@users.noreply.github.com>
2025-06-02 02:27:53 +0000

    05_strings.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -36,3 +36,34 @@ const myFunction = function() {
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//*******************************************
+
+//Stack(Primitive)Get a copy value, Heap(Non Primitive)cGet a original value
+
+let myYoutubename = "harshitsaxena.com"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com" ,
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "harshit@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
