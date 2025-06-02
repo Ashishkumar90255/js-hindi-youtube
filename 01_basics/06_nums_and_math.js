d6d29956f7b6cd24f0b922a02b12cb23610ce280
Ashishkumar90255 <143105783+Ashishkumar90255@users.noreply.github.com>
2025-06-02 09:31:35 +0000

    06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,24 @@
+//+++++++++++++++ NUMBERS +++++++++++++++//
+
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+
+// console.log(balance.toFixed(3));
+
+const otherNumber = 126.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++ MATHS ++++++++++++++++++//
+
+console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.abs(4));
